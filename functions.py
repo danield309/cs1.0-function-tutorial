09186b117b9ce5ddaf4173cca4b976042d63d496
daniel <duque1daniel@gmail.com>
2020-09-08 12:56:46 -0700

    Q4, Q5

--- functions.py
+++ functions.py
@@ -8,3 +8,11 @@ result2 = area(6, 7)
 print(result2)
 result3 = area(9, 10)
 print(result3)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+result = subtract(5, 2)
+print(result)
+result2 = subtract(10, 8)
+print(result2)
